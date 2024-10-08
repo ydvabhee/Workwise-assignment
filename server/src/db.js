eb1fda5fcf3ae0c600685e805cacc13e7c005600
Abhishek Yadav <yk.abhee@gmail.com>
2024-10-09 02:55:10 +0530

    added db url

--- server/src/db.js
+++ server/src/db.js
@@ -7,11 +7,17 @@ const USERNAME = process.env.DB_USERNAME
 const PASSWORD = process.env.DB_PASSWORD
 const HOST = process.env.DB_HOST
 const DIALECT = process.env.DB_DIALECT
+const DB_PORT = process.env.DB_PORT
+
+const DB_URL = process.env.DB_URL
 
 
-const sequelize = new Sequelize(DATABASE, USERNAME, PASSWORD, {
-  host: HOST,
-  dialect: DIALECT,
-});
+
+const db_url = DB_URL ? DB_URL : `postgresql://${USERNAME}:${PASSWORD}@${HOST}:${DB_PORT}/${DATABASE}`
+
+console.log(db_url)
+const sequelize = new Sequelize(db_url) // Example for postgres
+
+
 
 module.exports = sequelize
